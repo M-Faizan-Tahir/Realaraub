6dde890dc8a3ea2733e384e8c0d82d1cfc2fcfcf
M Faizan Tahir <faizant362@gmail.com>
2024-06-03 04:45:49 +0500

    intial models and migrations created

---
+++ database/migrations/2024_06_02_232256_create_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('order_number');
+            $table->string('status');
+            $table->string('grand_total');
+            $table->string('item_count');
+            $table->text('notes')->nullable();
+            $table->string('payment_status');
+            $table->string('payment_method');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
